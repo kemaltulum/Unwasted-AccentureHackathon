4ec9a4cc23a926d0d5adc453f0c78f18b90c0930
nazmicancalik <nazmicancalik@gmail.com>
2017-11-12 01:25:00 +0300

    Scoreboard component is added.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,23 +5,24 @@ import { UserComponent } from './user/user.component';
 import { HomeComponent } from './home/home.component';
 import { FeedbackComponent } from './feedback/feedback.component';
 import { ProfileComponent } from './profile/profile.component';
+import { ScoreboardComponent } from './scoreboard/scoreboard.component';
 
 
 const appRoutes: Routes = [
-  { path: 'home', component: HomeComponent},
-  { path: 'user', component: UserComponent},
-  { path: 'feedback/:id', component: FeedbackComponent},
-  { path: 'profile', component: ProfileComponent},
-  { path: '**', component: HomeComponent}
+  { path: 'home', component: HomeComponent },
+  { path: 'user', component: UserComponent },
+  { path: 'feedback/:id', component: FeedbackComponent },
+  { path: 'profile', component: ProfileComponent },
+  { path: 'scoreboard', component: ScoreboardComponent },
+  { path: '**', component: HomeComponent }
 ];
 
 @NgModule({
   imports: [
-    RouterModule.forRoot(appRoutes, {preloadingStrategy: PreloadAllModules})
+    RouterModule.forRoot(appRoutes, { preloadingStrategy: PreloadAllModules })
   ],
   exports: [RouterModule]
 
 })
-export class AppRoutingModule{
-
+export class AppRoutingModule {
 }
--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -1,9 +1,8 @@
-import { EventEmitter, Output, Injectable} from '@angular/core';
+import { EventEmitter, Output, Injectable } from '@angular/core';
 
 export class DataService {
 
   userScore = 0;
-  
   sampleMenu = [
     'Yoğurt Çorbası',
     'Adana Kebap',
@@ -13,26 +12,22 @@ export class DataService {
 
 
   userProfile = {
-    'username' : '',
-    'email' : '',
+    'username': '',
+    'email': '',
     'gender': '',
     'eggAllergy': '',
     'glutenAllergy': '',
     'cornAllergy': '',
   };
   sampleMenuImages = [
-      './assets/menu1.jpg',
-      './assets/menu2.jpg',
-      './assets/menu3.jpg',
-      './assets/menu4.jpg'
+    './assets/menu1.jpg',
+    './assets/menu2.jpg',
+    './assets/menu3.jpg',
+    './assets/menu4.jpg'
   ];
 
   constructor() { }
 
-<<<<<<< HEAD
-  setUserProfile(name, emai) {
-    this.userProfile.username = name;
-=======
   setUserProfile(username, email, gender, eggAllergy, glutenAllergy, cornAllergy) {
     this.userProfile.username = username;
     this.userProfile.email = email;
@@ -40,7 +35,7 @@ export class DataService {
     this.userProfile.eggAllergy = eggAllergy;
     this.userProfile.glutenAllergy = glutenAllergy;
     this.userProfile.cornAllergy = cornAllergy;
->>>>>>> d569b84354202d7cbbdc903c5cf09103d5df78a3
+
   }
 
   getFoodName(id) {
@@ -51,3 +46,4 @@ export class DataService {
     return this.sampleMenuImages[id];
   }
 }
+
