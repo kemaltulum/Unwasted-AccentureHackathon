dfe8c279615608bf6334b05beca01073bc5a23e0
nazmicancalik <nazmicancalik@gmail.com>
2017-11-11 21:24:59 +0300

    Forms module is added.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { DataService } from './services/data.service';
 import { AppRoutingModule } from './app-routing.module';
@@ -21,7 +22,8 @@ import { ProfileComponent } from './profile/profile.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [DataService],
   bootstrap: [AppComponent]
