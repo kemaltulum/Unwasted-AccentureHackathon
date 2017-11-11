12e4b9f1871d0fa210b5284b9a19b7bbfe832879
nazmicancalik <nazmicancalik@gmail.com>
2017-11-11 23:28:35 +0300

    Profile forms.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatToolbarModule } from '@angular/material';
-import { FormsModule } from '@angular/forms';
+import { MatToolbarModule, MatSelectModule, MatInputModule, MatButtonModule } from '@angular/material';
+import { FormsModule, ReactiveFormsModule} from '@angular/forms';
 
 import { DataService } from './services/data.service';
 import { AppRoutingModule } from './app-routing.module';
@@ -29,7 +29,11 @@ import { ProfileComponent } from './profile/profile.component';
     AppRoutingModule,
     BrowserAnimationsModule,
     MatToolbarModule,
-    FormsModule
+    MatSelectModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatInputModule,
+    MatButtonModule
   ],
   providers: [DataService],
   bootstrap: [AppComponent]
