cd4bdbf2131085a5b557b5da31b570302f7dee07
kemaltulum <tulum.ke@gmail.com>
2017-11-12 10:05:44 +0300

    small change

--- src/app/menu-evaluation/menu-evaluation.component.ts
+++ src/app/menu-evaluation/menu-evaluation.component.ts
@@ -3,8 +3,7 @@ import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 @Component({
   selector: 'app-menu-evaluation',
   templateUrl: './menu-evaluation.component.html',
-  styleUrls: ['./menu-evaluation.component.css'],
-  encapsulation: ViewEncapsulation.None
+  styleUrls: ['./menu-evaluation.component.css']
 })
 export class MenuEvaluationComponent implements OnInit {
 
